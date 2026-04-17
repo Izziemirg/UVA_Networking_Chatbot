b99f34e1e6a58515566a72090101ff465981e03e
Izzie Mirghani <Mirghani42@gmail.com>
2026-04-17 14:35:03 -0400

    Add project disclaimer to app.py
    Added a disclaimer about the project being an independent student initiative.

--- app.py
+++ app.py
@@ -383,6 +383,11 @@ with st.sidebar:
     
     st.markdown("---")
     
+    st.info(
+        "**Student Project Disclaimer:** This networking tool is an independent "
+        "student project created for the MSBA 2026 cohort. It is not an official "
+        "University of Virginia, Darden, or McIntire application."
+    )
     st.markdown("**How to use this chatbot:**")
     st.markdown("""
     Ask me questions like:
